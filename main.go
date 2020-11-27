a3e8386ccbcd658ba9a5c682c3f84ea9ead69b3d
fabeirojorge-sw <fabeirojorge.secondwindow@gmail.com>
2020-11-27 09:26:19 +0100

    Improved argument control regarding SSH authentication methods

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"net/http"
-	"os"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -23,7 +22,7 @@ var (
 	scheduler = flag.String(
 		"scheduler",
 		"pbs",
-		"Type of scheduler: pbs",
+		"Type of scheduler",
 	)
 	host = flag.String(
 		"host",
@@ -35,19 +34,25 @@ var (
 		"",
 		"SSH user for remote frontend connection (no localhost).",
 	)
+	sshAuthMethod = flag.String(
+		"ssh-auth-method",
+		"keypair",
+		"SSH authentication method for remote frontend connection (no localhost).",
+	)
 	sshPass = flag.String(
 		"ssh-password",
 		"",
 		"SSH password for remote frontend connection (no localhost).",
 	)
+
 	sshPrivKey = flag.String(
 		"ssh-private-key",
-		"~/.ssh/id_rsa",
+		"",
 		"SSH private key recognised by the remote frontend",
 	)
 	sshKnownHosts = flag.String(
 		"ssh-known-hosts",
-		"~/.ssh/known_hosts",
+		"",
 		"SSH local known_hosts file including the remote frontend",
 	)
 	countryTZ = flag.String(
@@ -79,33 +84,44 @@ func main() {
 	if *host == "localhost" {
 		flag.Usage()
 		log.Fatalln("Localhost connection not implemented yet.")
-	} else {
+	} else {		
 		if *sshUser == "" {
 			flag.Usage()
 			log.Fatalln("A user must be provided to connect to a frontend remotely.")
 		}
-		if *sshPass == "" {
-			flag.Usage()
-			log.Warnln("A password should be provided to connect to a frontend remotely.")
+		switch authmethod := *sshAuthMethod; authmethod {
+			case "keypair":
+				if (*sshPrivKey == "" || *sshKnownHosts == "") {
+					flag.Usage() 
+					log.Fatalln("Paths to a private key and a known hosts file should be provided to connect to a frontend remotely using this authentication method.")
+				}
+			case "password":
+				if (*sshPass == "") {
+					flag.Usage() 
+					log.Fatalln("A password should be provided to connect to a frontend remotely using this authentication method.")
+				}
+			default:
+				flag.Usage()
+				log.Fatalf("The authentication method provided (%s) is not supported.", authmethod)
 		}
 	}
-
-//	prometheus.MustRegister(NewerTorqueCollector(*host, *sshUser, *sshPass, *countryTZ))
+	
 	switch sched := *scheduler; sched {
-/*
-        case "torque":
-		log.Debugf("Registering collector for scheduler %s", sched)
-		prometheus.MustRegister(torque.NewerTorqueCollector(*host, *sshUser, *sshPass, *countryTZ))
-*/
-        case "pbs":
-		log.Debugf("Registering collector for scheduler %s", sched)
-		prometheus.MustRegister(pbs.NewerPBSCollector(*host, *sshUser, *sshPass, *sshPrivKey, *sshKnownHosts, *countryTZ))
-        default:
-                os.Exit(-1)
-        }
-
+	/*
+		case "torque":
+			log.Debugf("Registering collector for scheduler %s", sched)
+			prometheus.MustRegister(torque.NewerTorqueCollector(*host, *sshUser, *sshPass, *countryTZ))
+	*/
+		case "pbs":
+			log.Debugf("Registering collector for scheduler %s", sched)
+			prometheus.MustRegister(pbs.NewerPBSCollector(*host, *sshUser, *sshPass, *sshPrivKey, *sshKnownHosts, *countryTZ))
+		default:
+			log.Fatalf("The scheduler type provided (%s) is not supported.", sched)
+	}
+	
 	// Expose the registered metrics via HTTP.
 	log.Infof("Starting Server: %s", *addr)
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
+
